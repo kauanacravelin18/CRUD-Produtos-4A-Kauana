2b9d95aac494d1dff544169bd77d686ca3115848
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-11 11:02:37 -0300

    Aula5

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categoria;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Categoria::create([
+            'nome' => 'Informática'
+        ]);
+
+        Categoria::create([
+            'nome' => 'Comida'
+        ]);
+
+        Categoria::create([
+            'nome' => 'Carro'
+        ]);
+    }
+}
