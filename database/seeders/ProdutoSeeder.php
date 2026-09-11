15aad71c79cfae8269da18cfea6845ad8ff67d10
Kauana Cravelin <engs-kauanacravelin@camporeal.edu.br>
2026-09-11 10:34:19 -0300

    Importação inicial do projeto

---
+++ database/seeders/ProdutoSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Produto;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Produto::create([
+            'nome' => 'Notebook',
+            'preco' => 6000.50,
+            'quantidade' => 12,
+        ]);
+
+        Produto::create([
+            'nome' => 'Pizza',
+            'preco' => 47.50,
+            'quantidade' => 25,
+        ]);
+
+        Produto::create([
+            'nome' => 'Lajota',
+            'preco' => 21.90,
+            'quantidade' => 11,
+        ]);
+
+
+    }
+}
